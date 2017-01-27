022331aa858590ce158b41f30fc92b63f33af18f
Justin Staubach <justin@staubach.us>
2017-01-27 09:43:16 -0500

    import db_conf

--- webapp.py
+++ webapp.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 from configparser import ConfigParser
 from builtins import input
 import mysql.connector
+import db_conf
 
 #
 # import sys
@@ -33,22 +34,23 @@ def get_list(list_name, params):
 
     return_vals = OrderedDict()
 
-    cnx = mysql.connector.connect(user=db_conf.settings['DB']['db_user'],
-                                  password=db_conf.settings['DB']['db_pass'],
-                                  host=db_conf.settings['DB']['db_user']+'.mysql.pythonanywhere-services.com',
-                                  database=db_conf.settings['DB']['db_user'] + '$' + db_conf.settings['DB']['db_name'])
-    cursor = cnx.cursor()
+    # cnx = mysql.connector.connect(user=db_conf.settings['DB']['db_user'],
+    #                               password=db_conf.settings['DB']['db_pass'],
+    #                               host=db_conf.settings['DB']['db_host'],
+    #                               database=db_conf.settings['DB']['db_user'] + '$' + db_conf.settings['DB']['db_name'])
+    # cursor = cnx.cursor()
+    #
+    # query = ("SELECT * FROM " + list_name + "_tbl" )
+    #
+    #
+    # cursor.execute(query)
+    #
+    # for (participant_name) in cursor:
+    #     print("{}".format(participant_name))
+    #
+    # cursor.close()
+    # cnx.close()
 
-    query = ("SELECT * FROM " + list_name + "_tbl" )
-
-
-    cursor.execute(query)
-
-    for (participant_name) in cursor:
-        print("{}".format(participant_name))
-
-    cursor.close()
-    cnx.close()
     return_vals['test1'] = 'Hello World'
     return return_vals
 
@@ -69,40 +71,35 @@ class HoppersWebService(object):
     def DELETE(self, **kwargs):
         return 'DELETE:/hoppers/' + str(kwargs)
 
-class ws():
-    def __init__(self):
-        db_conf = DBConfig()
-        if not 'DB' in db_conf.settings.keys():
-            db_conf.settings['DB'] = {}
-        if not 'db_name' in db_conf.settings['DB'].keys():
-            db_conf.settings['DB']['db_name'] = input('db_name:')
-        if not 'db_user' in db_conf.settings['DB'].keys():
-            db_conf.settings['DB']['db_user'] = input('db_user:')
-        if not 'db_pass' in db_conf.settings['DB'].keys():
-            db_conf.settings['DB']['db_pass'] = getpass.getpass('Password:')
-        print("name {}".format(db_conf.settings['DB']['db_name']))
-        print("user {}".format(db_conf.settings['DB']['db_user']))
-        cherrypy.tree.mount(
-            HoppersWebService(),
-            '/hoppers',
-            {
-                '/': {
-                    'request.dispatch': cherrypy.dispatch.MethodDispatcher()
-                },
-            }, )
-        #     cherrypy.tree.mount(
-        #         Root(),
-        #         '/',
-        #         {
-        #             '/': {
-        #                 'tools.sessions.on': True,
-        #                 'tools.staticdir.root': os.path.abspath(os.getcwd())
-        #             }
-        #         }
-        #     )
-        cherrypy.config.update({'server.socket_port': 80})
-        cherrypy.engine.start()
-        cherrypy.engine.block()
-
 if __name__ == '__main__':
-    foo = ws()
+    # db_conf = DBConfig()
+    # if not 'DB' in db_conf.settings.keys():
+    #     db_conf.settings['DB'] = {}
+    # if not 'db_name' in db_conf.settings['DB'].keys():
+    #     db_conf.settings['DB']['db_name'] = input('db_name:')
+    # if not 'db_user' in db_conf.settings['DB'].keys():
+    #     db_conf.settings['DB']['db_user'] = input('db_user:')
+    # if not 'db_pass' in db_conf.settings['DB'].keys():
+    #     db_conf.settings['DB']['db_pass'] = getpass.getpass('Password:')
+    print("name {}".format(db_conf.settings['DB']['db_name']))
+    print("user {}".format(db_conf.settings['DB']['db_user']))
+    cherrypy.tree.mount(
+        HoppersWebService(),
+        '/hoppers',
+        {
+            '/': {
+                'request.dispatch': cherrypy.dispatch.MethodDispatcher()
+            },
+        }, )
+    #     cherrypy.tree.mount(
+    #         Root(),
+    #         '/',
+    #         {
+    #             '/': {
+    #                 'tools.sessions.on': True,
+    #                 'tools.staticdir.root': os.path.abspath(os.getcwd())
+    #             }
+    #         }
+    #     )
+    cherrypy.engine.start()
+    cherrypy.engine.block()
