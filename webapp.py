68244d9d3bb4f108eda4429a2611098ed6ce3d66
Justin Staubach <justin@staubach.us>
2017-01-30 13:54:23 -0500

    Do CRUD.

--- webapp.py
+++ webapp.py
@@ -16,33 +16,69 @@ if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
     cherrypy.engine.start(blocking=False)
     atexit.register(cherrypy.engine.stop)
 
+
 def get_list(list_name, params):
-    print('get_list list:{} params:{}'.format(str(list_name), str(params)))
-    print(db_conf.settings['DB']['db_user'])
-
-    print(db_conf.settings['DB']['db_user'] + '$' + db_conf.settings['DB']['db_name'])
-
     return_vals = []
-
     cnx = mysql.connector.connect(user=db_conf.settings['DB']['db_user'],
                                   password=db_conf.settings['DB']['db_pass'],
                                   host=db_conf.settings['DB']['db_host'],
-                                  database=db_conf.settings['DB']['db_user'] + '$' + db_conf.settings['DB'][
-                                      'db_name'])
+                                  database=db_conf.settings['DB']['db_user'] + '$' + db_conf.settings['DB']['db_name'])
     cursor = cnx.cursor(dictionary=True)
-    query = ("SELECT * FROM " + list_name + "s_tbl")
+    query = ("SELECT * FROM " + list_name)
     cursor.execute(query)
-
     for row in cursor:
         return_vals.append(dict(row))
-        print(str(row[list_name + '_id']) + ' ' + str(row[list_name + '_name']))
-
     cursor.close()
     cnx.close()
-
-    print(str(return_vals))
-
     return return_vals
+
+
+def remove_row(list_name, params):
+    cnx = mysql.connector.connect(user=db_conf.settings['DB']['db_user'],
+                                  password=db_conf.settings['DB']['db_pass'],
+                                  host=db_conf.settings['DB']['db_host'],
+                                  database=db_conf.settings['DB']['db_user'] + '$' + db_conf.settings['DB']['db_name'])
+    cursor = cnx.cursor(dictionary=True)
+    cmd = "DELETE FROM " + list_name + "_tbl WHERE " + list_name + "_tbl." + list_name[:-1] + "_id = " + params[0]
+    query = cmd
+    cursor.execute(query)
+    cursor.close()
+    cnx.commit()
+    cnx.close()
+    return {'method': 'delete', 'status': 'success'}
+
+
+def create_row(new_data, list_name):
+    cnx = mysql.connector.connect(user=db_conf.settings['DB']['db_user'],
+                                  password=db_conf.settings['DB']['db_pass'],
+                                  host=db_conf.settings['DB']['db_host'],
+                                  database=db_conf.settings['DB']['db_user'] + '$' + db_conf.settings['DB']['db_name'])
+    cursor = cnx.cursor(dictionary=True)
+    cmd = "INSERT INTO " + list_name + "_tbl (" + ",".join(new_data.keys())+") VALUES (" + ",".join([ "'" + new_data[key] + "'" for key in new_data]) + ")"
+    query = cmd
+    cursor.execute(query)
+    cursor.close()
+    cnx.commit()
+    cnx.close()
+    return {'method': 'post', 'status': 'success'}
+
+
+def update_row(new_data, list_name, params):
+    cnx = mysql.connector.connect(user=db_conf.settings['DB']['db_user'],
+                                  password=db_conf.settings['DB']['db_pass'],
+                                  host=db_conf.settings['DB']['db_host'],
+                                  database=db_conf.settings['DB']['db_user'] + '$' + db_conf.settings['DB']['db_name'])
+    cursor = cnx.cursor(dictionary=True)
+
+    cmd = "UPDATE " + list_name + "_tbl SET " + ','.join([key + " = '" + new_data[key] + "'" for key in new_data.keys()]) + " WHERE " + list_name + "_tbl." + list_name[:-1] + "_id = " + params[0]
+    print(cmd)
+    query = cmd
+    cursor.execute(query)
+    cursor.close()
+    cnx.commit()
+    cnx.close()
+    return {'method': 'delete', 'status': 'success'}
+
 
 
 class HoppersWebService(object):
@@ -53,28 +89,28 @@ class HoppersWebService(object):
         if not args:
             args = [None, None]
         if args[0] == 'hoppers' and args[1] == 'rest':
-            return json.dumps(get_list(args[2], args[3:]) + cherrypy.request.scheme)
+            return json.dumps(get_list(args[2], args[3:]))
 
     def POST(self, *args):
         print('POST '+str(args)+cherrypy.request.scheme)
         rawData = cherrypy.request.body.read(int(cherrypy.request.headers['Content-Length']))
-        b = json.loads(rawData)
-        print('post data: '+str(b))
-        return json.dumps('POST:/hoppers/' + str(args) + cherrypy.request.scheme)
+        new_data = json.loads(rawData)
+        print('post data: '+str(new_data))
+        if args[0] == 'hoppers' and args[1] == 'rest':
+            return json.dumps(create_row(new_data, args[2]))
 
     def PUT(self, *args):
         print('PUT ' + str(args)+cherrypy.request.scheme)
         rawData = cherrypy.request.body.read(int(cherrypy.request.headers['Content-Length']))
-        b = json.loads(rawData)
-        print('put data: ' + str(b))
-        return json.dumps('PUT:/hoppers/' + str(args) + cherrypy.request.scheme)
+        new_data = json.loads(rawData)
+        print('put data: ' + str(new_data))
+        if args[0] == 'hoppers' and args[1] == 'rest':
+            return json.dumps(update_row(new_data, args[2], args[3:]))
 
     def DELETE(self, *args):
         print('DELETE ' + str(args)+cherrypy.request.scheme)
-        # rawData = cherrypy.request.body.read(int(cherrypy.request.headers['Content-Length']))
-        # b = json.loads(rawData)
-        # print('delete data: ' + str(b))
-        return json.dumps('DELETE:/hoppers/' + str(args) + cherrypy.request.scheme)
+        if args[0] == 'hoppers' and args[1] == 'rest':
+            return json.dumps(remove_row(args[2], args[3:]))
 
     def serve_index(self):
         print('index'+cherrypy.request.scheme)
